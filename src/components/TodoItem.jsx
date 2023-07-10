45d1b45f0be0cc64fcc3620a65c934a6611ea209
Muhammad Amran Khan (Mike) <Tipu870@gmail.com>
2023-07-10 23:15:50 +0500

    Update TodoItem.jsx

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,5 +1,14 @@
+import PropTypes from 'prop-types';
+
 const TodoItem = ({ itemProp }) => {
-    return <li>{itemProp.title}</li>;
-  };
-  export default TodoItem;
-  
+  return <li>{itemProp.title}</li>;
+};
+
+TodoItem.propTypes = {
+  itemProp: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    // Add any other required properties of the `itemProp` object here
+  }).isRequired,
+};
+
+export default TodoItem;
