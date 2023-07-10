4b92ba0fd816be8f2920799bf39c1d55402829b9
Muhammad Amran Khan (Mike) <Tipu870@gmail.com>
2023-07-10 23:16:08 +0500

    Update TodoList.jsx

--- src/components/TodosList.jsx
+++ src/components/TodosList.jsx
@@ -1,13 +1,24 @@
+import PropTypes from 'prop-types';
 import TodoItem from '@/components/TodoItem';
+
 const TodosList = (props) => {
-    const { todosProps } = props;
-    return (
-      <ul>
-        {todosProps.map((todo) => (
-          <TodoItem key={todo.id} itemProp={todo} />
-        ))}
-      </ul>
-    );
-  };
-  export default TodosList;
-  
+  const { todosProps } = props;
+  return (
+    <ul>
+      {todosProps.map((todo) => (
+        <TodoItem key={todo.id} itemProp={todo} />
+      ))}
+    </ul>
+  );
+};
+
+TodosList.propTypes = {
+  todosProps: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      // Add any other required properties of the `todo` object here
+    })
+  ).isRequired,
+};
+
+export default TodosList;
